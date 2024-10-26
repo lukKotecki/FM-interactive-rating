7eea0d7d462a72101fa236e7fc3e9b8d5c7a9687
Lukas <lukkotecki@gmail.com>
2024-10-26 05:56:33 +0200

    typescript logic implementation startet

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,16 @@ var _a;
 console.log('hel lo');
 let radioInput = document.getElementsByClassName('radio-input');
 console.log(radioInput);
-console.log("costam");
 (_a = document.getElementById("submit")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function (e) {
     e.preventDefault();
     console.log("submit button clicked");
+});
+// radioInput.forEach(radio => {
+//   radio.addEventListener("click", function(e) {
+//     console.log(e.target.value)
+//   })
+// })
+radioInput[0].addEventListener("click", function (e) {
+    console.log(e.target);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,18 @@ let radioInput = document.getElementsByClassName('radio-input')
 console.log(radioInput)
 
 
-console.log("costam")
-
 document.getElementById("submit")?.addEventListener("click", function(e) {
     e.preventDefault()
   console.log("submit button clicked")
 })
 
+// radioInput.forEach(radio => {
+//   radio.addEventListener("click", function(e) {
+//     console.log(e.target.value)
+//   })
+// })
+
+
+radioInput[0].addEventListener("click", function(e) {
+  console.log(e.target)
+})
